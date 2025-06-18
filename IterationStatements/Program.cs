6ffda3dd79e4a17852e56e95652239d3381c9ee8
Scott Dyrness <scottdyrness@gmail.com>
2025-06-17 20:57:25 -0700

    Completed Iteration Statements Project

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -17,9 +17,9 @@ namespace IterationStatements
 
             //TODO - Create a variable of type int and name it "num"
             //TODO - Initialize the variable with a value of 0
-
-
-
+            int num = 0;
+            
+            
             //TODO - Create a do-while loop using the existing brackets as a template.
             // The existing brackets are placeholders for your loop code.
             // Inside this loop:
@@ -27,10 +27,12 @@ namespace IterationStatements
             // b) Then add "num" to the collection "numbers"
             //    Hint: Use "numbers.Add(num);" to add the current value of "num" to the list.
 
+            do
             {
+                num++;
                 // Your code goes here
-                numbers.Add(/*num*/); //uncomment out `num` to add it to the collection "numbers"
-            } 
+                numbers.Add(num); //uncomment out `num` to add it to the collection "numbers"
+            } while (num < 100);
             //TODO - Continue the loop while "num" is less than 100
             
 
@@ -41,9 +43,10 @@ namespace IterationStatements
             // a) Increment "num" by 1
             // b) Then add "num" to the collection "numbers"
             //    Hint: You can copy how this was done in the do-while loop
-
+            while (num < 200)
             {
-                // Your code goes here
+                num++; // Your code goes here
+                numbers.Add(num);
             }
             
 
@@ -53,10 +56,11 @@ namespace IterationStatements
 
 
             //TODO - Create a foreach loop using the existing brackets as a template.
-            // The existing brackets are placeholders for your loop code.
-            // Inside this loop, print each number in "numbers".
+            //The existing brackets are placeholders for your loop code.
+            //Inside this loop, print each number in "numbers".
+            foreach (var number in numbers)
             {
-                // Your code goes here
+               Console.WriteLine(number); // Your code goes here
             }
              
 
@@ -74,7 +78,7 @@ namespace IterationStatements
             //    - Less than or equal to the number of items in "numbers" (use "numbers.Count")
             //    - Greater than or equal to 0
             // c) Decrement "i" by 1 in each iteration
-
+            for(int i =199; i<= numbers.Count && i >= 0; i--)
             {
                 //TODO - Inside the loop, place numbers[i] inside of the Console.WriteLine() method
                 Console.WriteLine(numbers[i]); // Example placement of numbers[i] inside Console.WriteLine
